f7cf2e3c7593562b54dd0de4021de10a20aa406c
Jesse Gardner <jesse@plasticmind.com>
2013-11-13 13:55:50 -0500

    Added width alert for tracking media query changes

--- m/header.php
+++ m/header.php
@@ -9,6 +9,21 @@
 	<meta http-equiv="Content-type" content="text/html; charset=utf-8">
 	<link rel="stylesheet" href="style.css">
 	<?php echo (isset($header_meta)?$header_meta:''); ?>
+	<script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+	<script>
+		$( document ).ready(function() {
+			var body_size = $('#wrap').width();
+			$("#wrap").prepend('<div id="width-logger">'+body_size+'px</div>');
+			var resizeTimer;
+			$(window).resize(function() {
+			  clearTimeout(resizeTimer);
+			  resizeTimer = setTimeout(function() {
+			    var body_size = $('#wrap').width();
+			    $('#width-logger').html(body_size+'px');
+			  }, 25);
+			});
+		});
+	</script>
 </head>
 <body class="patterns">
 	<?php include(INCLUDE_ROOT . 'hud.php'); ?>
