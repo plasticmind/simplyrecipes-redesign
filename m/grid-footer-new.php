bbab3d353fb47fa9b445b2ba87b4208a53dd4f2e
Jesse Gardner <jesse@plasticmind.com>
2013-11-14 14:29:44 -0500

    Adding header/footer files for new layout

---
+++ m/grid-footer-new.php
@@ -0,0 +1,20 @@
+		<aside class="sidebar sidebar-left">
+			<div class="widget widget-nav"><p>Nav: Recipe Types</p></div>
+			<div class="widget widget-fbs"><p>Widget: FB Search</p></div>
+			<div class="widget widget-foodblogs"><p>Widget: Food Blogs</p></div>
+			<div class="widget widget-converter"><p>Widget: Converter</p></div>
+			<div class="widget widget-ad-mobile"><p>Ad: Mobile Site</p></div>
+			<div class="widget widget-discover"><p>Widget: Discover</p></div>
+			<div class="widget"><p>Widget</p></div>
+		</aside>
+		<aside class="sidebar sidebar-right">
+			<div class="widget widget-social"><p>Widget: Social Links</p></div>
+			<div class="widget widget-ad-tall"><p>Ad: BlogHer</p></div>
+			<div class="widget widget-ad-tall"><p>Ad: Secondary</p></div>
+			<div class="widget widget-recent"><p>Widget: Recent Posts</p></div>
+			<div class="widget widget-ad-house"><p>Ad: House</p></div>
+			<div class="widget widget-facebook"><p>Widget: Facebook</p></div>
+		</aside>
+	</div>
+
+	<footer class="site-footer">Footer</footer>
---
+++ m/grid-header-new.php
@@ -0,0 +1,14 @@
+	<header class="site-header">
+		<div class="site-brand">Brand</div>
+		<div class="site-search">Search</div>
+		<nav class="site-nav">
+			<ul>
+				<li>Item1</li>
+				<li>Item2</li>
+				<li>Item3</li>
+				<li>Item4</li>
+				<li>Item5</li>
+			</ul>
+		</nav>
+	</header>
+	<div class="page-content">
